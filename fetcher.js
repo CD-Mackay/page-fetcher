c3b2eb353653fcdc4fa13d563262cb8c38341f00
CD-Mackay <connor.mackay@gmail.com>
2020-12-03 00:43:57 +0000

    implemented functionality to retrieve and save data from webpage

--- fetcher.js
+++ fetcher.js
@@ -1,24 +1,32 @@
 const fs = require('fs');
 const request = require('request');
 let arguments = process.argv.slice(2);
+let file = arguments[1];
 //initialize data variable
 let data;
-// Reqeust data from server
+// Request data from server
+const makeFile = function(data) {
+  fs.writeFile(file, data, function(err) {
+    if (err) return console.log(err);
+  });
+};
+
 request(arguments[0], (error, response, body) => {
   if (error) {
-  console.log('Error: ', error);
+    console.log('Errors: ', error);
   }
-  // console.log('StatusCode:', response.statusCode);
-  // console.log('Body: ', body);
+   else if (response.statusCode !== 200) {
+     console.log("Something has gone terribly awry");
+   }
+   else console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${file}`);
+   
+
+
   // Save html body as data variable
   data = body;
-  console.log("Data: ", data);
+  makeFile(data);
 });
-console.log("Data: ", data);
 
-let file = arguments[1];
-fs.writeFile(file, data, function(err) {
-  if (err) return console.log(err);
-  console.log("Data added!");
-  console.log(`${data} >>> ${file}`);
-});
+
+
+
