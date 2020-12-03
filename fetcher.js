0ceb3054444c7ebd5e6b1d9d35adbd5e8a34d1f4
CD-Mackay <connor.mackay@gmail.com>
2020-12-03 01:26:40 +0000

    refactored to show accurate error messages

--- fetcher.js
+++ fetcher.js
@@ -2,29 +2,22 @@ const fs = require('fs');
 const request = require('request');
 let arguments = process.argv.slice(2);
 let file = arguments[1];
-//initialize data variable
-let data;
-// Request data from server
-const makeFile = function(data) {
-  fs.writeFile(file, data, function(err) {
-    if (err) return console.log(err);
-  });
-};
 
 request(arguments[0], (error, response, body) => {
   if (error) {
-    console.log('Errors: ', error);
-  }
-   else if (response.statusCode !== 200) {
-     console.log("Something has gone terribly awry");
-   }
-   else console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${file}`);
-   
-
-
-  // Save html body as data variable
-  data = body;
-  makeFile(data);
+    // Error message if request fails
+    console.log('Error: URL is Invalid');
+  } else
+  fs.writeFile(file, body, function(err) {
+    if (err) {
+      // Error message for invalid file path
+      console.log("File path is invalid");
+      return ;
+    } else {
+      //Success message :)
+      console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${file}`);
+    }
+  });
 });
 
 
